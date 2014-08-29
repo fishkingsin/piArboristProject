fd9e8b60f01d4ba2f8ebc6e33458650d1de69f93
fishkingsin <fishkingsin@gmail.com>
2014-08-29 10:14:59 +0800

    fixed python syntax

--- analog_reader.py
+++ analog_reader.py
@@ -8,38 +8,38 @@ DEBUG = 1
 
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+		if ((adcnum > 7) or (adcnum < 0)):
+				return -1
+		GPIO.output(cspin, True)
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+		GPIO.output(clockpin, False)  # start clock low
+		GPIO.output(cspin, False)     # bring CS low
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+		commandout = adcnum
+		commandout |= 0x18  # start bit + single-ended bit
+		commandout <<= 3    # we only need to send 5 bits here
+		for i in range(5):
+				if (commandout & 0x80):
+						GPIO.output(mosipin, True)
+				else:
+						GPIO.output(mosipin, False)
+				commandout <<= 1
+				GPIO.output(clockpin, True)
+				GPIO.output(clockpin, False)
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+		adcout = 0
+		# read in one empty bit, one null bit and 10 ADC bits
+		for i in range(12):
+				GPIO.output(clockpin, True)
+				GPIO.output(clockpin, False)
+				adcout <<= 1
+				if (GPIO.input(misopin)):
+						adcout |= 0x1
 
-        GPIO.output(cspin, True)
-        
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
+		GPIO.output(cspin, True)
+		
+		adcout >>= 1       # first bit is 'null' so drop it
+		return adcout
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
@@ -59,26 +59,24 @@ potentiometer_adc = 0;
 
 last_read = 0       # this keeps track of the last potentiometer value
 tolerance = 5       # to keep from being jittery we'll only change
-                    # volume when the pot has moved more than 5 'counts'
+					# volume when the pot has moved more than 5 'counts'
 
 while True:
-        # we'll assume that the pot didn't move
-        trim_pot_changed = False
+		# we'll assume that the pot didn't move
+		trim_pot_changed = False
 
-        # read the analog pin
-        trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        # how much has it changed since the last read?
-        pot_adjust = abs(trim_pot - last_read)
+		# read the analog pin
+		trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+		# how much has it changed since the last read?
+		pot_adjust = abs(trim_pot - last_read)
+		if DEBUG:
+			print ("trim_pot : ",trim_pot)
+			# print "pot_adjust:", pot_adjust
+			# print "last_read", last_read
+		if( pot_adjust > tolerance ):
+			   trim_pot_changed = True
+		if( trim_pot_changed ):
+				last_read = trim_pot
 
-        if DEBUG:
-                print "trim_pot:", trim_pot
-                # print "pot_adjust:", pot_adjust
-                # print "last_read", last_read
-
-        if ( pot_adjust > tolerance ):
-               trim_pot_changed = True
-        if ( trim_pot_changed ):
-                last_read = trim_pot
-
-        # hang out and do nothing for a half second
-        time.sleep(0.1)
+		# hang out and do nothing for a half second
+		time.sleep(0.1)
--- playAudio.py
+++ playAudio.py
@@ -3,4 +3,4 @@ pygame.mixer.init()
 pygame.mixer.music.load("normal.mp3")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
-    continue
+	continue
